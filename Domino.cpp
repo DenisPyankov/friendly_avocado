961ba363d5e73763234e53ef1c29f909f3308a12
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-03-09 23:35:00 +0500

    Add files via upload

---
+++ Domino.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+class Dice {
+public:
+	int left;
+	int right;
+};
+
+class Domino {
+	int left;
+	int right;
+	
+	void Change_Dice(Dice My, int x, int y) {
+		My.left = x;
+		My.right = y;
+	};
+
+	void Enemy_left(Domino now, Dice Enemy) {
+		now.left = Enemy.right;
+	};
+
+	void Enemy_right(Domino now, Dice Enemy) {
+		now.right = Enemy.left;
+	};
+
+	void put(Dice My, Domino now) {
+
+		if (now.left == My.left)
+			now.left = My.right;
+		
+		if (now.left == My.right)
+			now.left = My.left;
+
+		if (now.right == My.left)
+			now.right = My.right;
+
+		if (now.right == My.right)
+			now.right = My.left;
+
+	};
+
+};
+
+int main()
+{
+	Domino p1;
+	return 0;
+}
