98f3f80055f06994d887bcc01757be8fa78cc8d7
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-03-09 22:07:36 +0500

    Add files via upload

---
+++ fool.cpp
@@ -0,0 +1,32 @@
+﻿#include <vector>
+#include <iostream>
+#include <string>
+using namespace std;
+
+class playing_card {
+public:
+	string card_suit;
+	int value;
+};
+
+class fool {
+public:
+	vector <playing_card> My_deck;
+	vector <playing_card> Enemy_deck;
+
+	void compare() {
+	};
+
+	void throwing_out() {
+	};
+
+	void add() {
+	};
+
+};
+
+int main()
+{
+	fool p1;
+	return 0;
+}
